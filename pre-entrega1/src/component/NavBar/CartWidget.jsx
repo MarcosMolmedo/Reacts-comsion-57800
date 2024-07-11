673f651545e6cd511048f297a85ec198c47edfda
¨Marcos <marcosmolmedo@gmail.com>
2024-07-11 23:40:38 +0200

    se agrega carrito desde libreria y queda planteada la barra de menu

---
+++ pre-entrega1/src/component/NavBar/CartWidget.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { IoCart } from "react-icons/io5";
+
+const CartWidget = () => {
+  return (
+    <div>
+      <IoCart size ={50} color="black" />
+     <p>1</p>
+    </div>
+  )
+}
+
+export default CartWidget
--- pre-entrega1/src/component/NavBar/NavBar.jsx
+++ pre-entrega1/src/component/NavBar/NavBar.jsx
@@ -1,13 +1,24 @@
 import logoMyTranslator from "../../img/logo.png"
+import CartWidget from "./CartWidget"
 
 const NavBar = () => {
     return (
      <nav>
         <div className="Brand">
-            <img src={logoMyTranslator} width={200} alt="" />
+            <img src={logoMyTranslator} width={150} alt="" />
            
          <h1>My Translator</h1>
        </div>
+       <ul>
+        <li>Traduccion Partida de Nacimiento Argentina</li>
+        <li>Traduccion Partida de Nacimiento Uruguaya</li>
+        <li>Traduccion Partida de Nacimiento Chilena</li>
+        <li>Traduccion Partida de Nacimiento Mexicana</li>
+        <li>Traduccion Partida de Nacimiento Espanola</li>
+        <li>Traduccion Partida de Nacimiento de otro pais Hispano Hablante</li>
+       </ul>
+
+       <CartWidget />
     </nav>
     )
   }
