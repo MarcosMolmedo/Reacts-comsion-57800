93c7e94d1a71a8831a9dfe3b48a4783fd401ef93
¨Marcos <marcosmolmedo@gmail.com>
2024-07-14 20:13:53 +0200

    se le da estilo a App.jsx queda funcional

--- pre-entrega1/src/App.jsx
+++ pre-entrega1/src/App.jsx
@@ -1,37 +1,89 @@
 import './App.css'
 import NavBar from "./component/NavBar/NavBar"
 import ItemListContainer from './component/itemListContainer/ItemListContainer'
+import argentina from './assets/img/argentina.png';
+import uruguay from './assets/img/banderauruguay.png'
+import chilena from './assets/img/banderachilena.png'
+import mexicana from './assets/img/banderamexicana.png'
+import espana from './assets/img/banderaespana.png'
+import mundo from './assets/img/banderasdelmundo.png'
 
 
 function App() {
- 
-
   return (
- 
-      <div >
-   
-        <NavBar/>    
-        <ItemListContainer saludo="Bienvenidos"  />
-        
-        <div> <h5 classNane="titulo">Tipo de Traducciones</h5></div>
-    
-        <ul className="tipodetraducciones">
-           <li>Traducción Partida de Nacimiento Argentina</li>
-            <li>Traducción Partida de Nacimiento Uruguaya</li>
-            <li>Traducción Partida de Nacimiento Chilena</li>
-            <li>Traducción Partida de Nacimiento Mexicana</li>
-            <li>Traducción Partida de Nacimiento Española</li>
-            <li>Traducción Partida de Nacimiento de otro país Hispano Hablante</li>
-          </ul>
-        
+    <div className="App">
+      <NavBar />
+      <main className="container">
+        <ItemListContainer saludo="Bienvenidos" />
+        <div><h4>Traducciones de partidas de nacimiento</h4></div>
 
-      </div>
+        <div className="row mt-4">
+          <div className="col-md-6">
+            <div className="row mb-3">
+              <div className="col-4">
+                <div className="rectangle">
+                  
+                </div>
+              </div>
+              <div className="col-8">
+              
+                <ul className="tipodetraducciones">
+                  <li>
+                    <div className="rectangular-img">
+                      <img src={argentina} alt="Argentina" className="img-fluid" />
+                    </div>
+                    Argentinas
+                  </li>
+                  <li>
+                    <div className="rectangular-img">
+                      <img src={uruguay} alt="bandera uruguay" className="img-fluid" />
+                    </div>
+                    Uruguayas
+                  </li>
+                  <li>
+                    <div className="rectangular-img">
+                      <img src={chilena} alt="bandera de chile" className="img-fluid" />
+                    </div>
+                    Chilenas
+                  </li>
+                </ul>
+              </div>
+            </div>
 
-      
-      
- 
-    
-  )
+            <div className="row mb-3">
+              <div className="col-4">
+                <div className="rectangle">
+                </div>
+              </div>
+              <div className="col-8">
+                <ul className="tipodetraducciones">
+                  <li>
+                    <div className="rectangular-img">
+                      <img src={mexicana} alt="bandera mexicana" className="img-fluid" />
+                    </div>
+                    Mexicanas
+                  </li>
+                  <li>
+                    <div className="rectangular-img">
+                      <img src={espana} alt="bandera espana" className="img-fluid" />
+                    </div>
+                    Españolas
+                  </li>
+                  <li>
+                    <div className="rectangular-img">
+                      <img src={mundo} alt="banderasdelmundo" className="img-fluid" />
+                    </div>
+                    Otros paises 
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+
+        </div>
+      </main>
+    </div>
+  );
 }
 
-export default App
+export default App;
