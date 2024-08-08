272274c24a2c9e7d7490392a4968908b7a8d0376
¨Marcos <marcosmolmedo@gmail.com>
2024-08-08 12:41:15 +0200

    agrego contenido relevante, se corrige la logica y sintaxis en ItemListContainer

--- pre-entrega1/src/component/itemListContainer/ItemListContainer.jsx
+++ pre-entrega1/src/component/itemListContainer/ItemListContainer.jsx
@@ -1,12 +1,37 @@
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
-// const {} = useParams (idtipodetraducciones) no la puedo ejecutar
-
-
 const ItemListContainer = ({ saludo }) => {
+  const [productos, setProductos] = useState([]); 
+  const { idtipodetraducciones } = useParams();
+
+  useEffect(() => {
+    const obtenerProductos = async () => {
+      try {
+        const dataProductos = await fetchProductos(); 
+
+        if (idtipodetraducciones) {
+          const productosFiltrados = dataProductos.filter(
+            (producto) => producto.categoria === idtipodetraducciones
+          );
+          setProductos(productosFiltrados);
+        } else {
+          setProductos(dataProductos);
+        }
+      } catch (error) {
+        console.error(error);
+      } finally {
+        console.log("Finalizó la promesa");
+      }
+    };
+
+    obtenerProductos();
+  }, [idtipodetraducciones]);
+
   return (
     <div>
       <h2>{saludo}</h2>
+      {/*profe, aca deberian ir el listaod de productos??? */}
     </div>
   );
 };
